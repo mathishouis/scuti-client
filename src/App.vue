f18e16fad01727914bd2e00071ed45f49dfa1e21
Mathis HOUIS <mathishouis@gmail.com>
2022-08-04 22:12:45 +0200

    Project initialisation

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    Scuti :D
+</template>
+
+<script>
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: "App"
+    });
+</script>
+
+<style scoped>
+
+</style>
